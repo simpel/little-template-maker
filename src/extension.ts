4806fd7244fcf2c63a6ed6660abaf4534cf972a7
Joel Sandén <me@joelsanden.se>
2022-11-30 13:49:34 +0100

    chore(debugging): fixing build

--- src/extension.ts
+++ src/extension.ts
@@ -7,103 +7,100 @@ import pickDirectory from './helpers/getDirectories/getDirectories';
 import pickTemplate from './helpers/pickTemplate/pickTemplate';
 import applyTemplate from './helpers/applyTemplate/applyTemplate';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
+const useTemplate = async (context: vscode.ExtensionContext) => {
+	const workspace = vscode.workspace.workspaceFolders
+		? vscode.workspace.workspaceFolders[0]
+		: undefined;
+	if (!workspace) {
+		void vscode.window.showErrorMessage("Couldn't find workspace");
+		return;
+	}
+
+	const templatesTargetPath = vscode.Uri.joinPath(workspace.uri, '.templates');
+
+	const templatesWorkspacePath = vscode.Uri.joinPath(
+		vscode.Uri.parse(context.extensionPath),
+		'.templates',
+	);
+	if (!existsSync(templatesTargetPath.fsPath)) {
+		await vscode.workspace.fs.copy(
+			templatesWorkspacePath,
+			templatesTargetPath,
+			{
+				overwrite: false,
+			},
+		);
+	}
+
+	const pickedTemplate = await pickTemplate(workspace).then((response) => {
+		if (response instanceof Error) {
+			void vscode.window.showErrorMessage(response.message);
+			return;
+		}
+
+		return response;
+	});
+
+	if (!pickedTemplate) {
+		return;
+	}
+
+	const pickedDirectory = await pickDirectory(workspace.uri).then(
+		(response) => {
+			if (response instanceof Error) {
+				void vscode.window.showErrorMessage(response.message);
+				return;
+			}
+
+			return response;
+		},
+	);
+
+	if (!pickedDirectory) {
+		return;
+	}
+
+	const templateVariables = await fetchTemplateVariables(pickedTemplate).then(
+		(response) => {
+			if (response instanceof Error) {
+				void vscode.window.showErrorMessage(response.message);
+				return;
+			}
+
+			return response;
+		},
+	);
+
+	let assignedVariables;
+	if (templateVariables) {
+		assignedVariables = await assignVariables(templateVariables).then(
+			(response) => {
+				if (response instanceof Error) {
+					void vscode.window.showErrorMessage(response.message);
+					return;
+				}
+
+				return response;
+			},
+		);
+	}
+
+	await applyTemplate(
+		workspace.uri,
+		pickedTemplate,
+		pickedDirectory,
+		assignedVariables,
+	).then(() => {
+		void vscode.window.showInformationMessage(
+			`Template applied successfully at ${pickedDirectory.fsPath}`,
+		);
+	});
+};
+
 export async function activate(context: vscode.ExtensionContext) {
 	const ext = vscode.commands.registerCommand(
 		'littleTemplateMaker.useTemplate',
-		async () => {
-			const workspace = vscode.workspace.workspaceFolders
-				? vscode.workspace.workspaceFolders[0]
-				: undefined;
-			if (!workspace) {
-				void vscode.window.showErrorMessage("Couldn't find workspace");
-				return;
-			}
-
-			const templatesTargetPath = vscode.Uri.joinPath(
-				workspace.uri,
-				'.templates',
-			);
-
-			const templatesWorkspacePath = vscode.Uri.joinPath(
-				vscode.Uri.parse(context.extensionPath),
-				'.templates',
-			);
-			if (!existsSync(templatesTargetPath.fsPath)) {
-				await vscode.workspace.fs.copy(
-					templatesWorkspacePath,
-					templatesTargetPath,
-					{
-						overwrite: false,
-					},
-				);
-			}
-
-			const pickedTemplate = await pickTemplate(workspace).then((response) => {
-				if (response instanceof Error) {
-					void vscode.window.showErrorMessage(response.message);
-					return;
-				}
-
-				return response;
-			});
-
-			if (!pickedTemplate) {
-				return;
-			}
-
-			const pickedDirectory = await pickDirectory(workspace.uri).then(
-				(response) => {
-					if (response instanceof Error) {
-						void vscode.window.showErrorMessage(response.message);
-						return;
-					}
-
-					return response;
-				},
-			);
-
-			if (!pickedDirectory) {
-				return;
-			}
-
-			const templateVariables = await fetchTemplateVariables(
-				pickedTemplate,
-			).then((response) => {
-				if (response instanceof Error) {
-					void vscode.window.showErrorMessage(response.message);
-					return;
-				}
-
-				return response;
-			});
-
-			let assignedVariables;
-			if (templateVariables) {
-				assignedVariables = await assignVariables(templateVariables).then(
-					(response) => {
-						if (response instanceof Error) {
-							void vscode.window.showErrorMessage(response.message);
-							return;
-						}
-
-						return response;
-					},
-				);
-			}
-
-			await applyTemplate(
-				workspace.uri,
-				pickedTemplate,
-				pickedDirectory,
-				assignedVariables,
-			).then(() => {
-				void vscode.window.showInformationMessage(
-					`Template applied successfully at ${pickedDirectory.fsPath}`,
-				);
-			});
-		},
+		async () => useTemplate(context),
 	);
 
 	context.subscriptions.push(ext);
