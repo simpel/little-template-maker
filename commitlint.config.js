e0de52c0176eb452c8e488860c09bf47718f0fe5
Joel Sandén <joel.sanden@regionhalland.se>
2022-11-30 09:49:57 +0100

    build(commitlint): added commitlint

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,8 +1,4 @@
 module.exports = {
 	"extends": ["@commitlint/config-conventional"],
-	"rules": {
-		"body-max-line-length": [0],
-		"body-max-length": [0],
-		"body-empty": [2, "never", 0]
-	}
+
 };
